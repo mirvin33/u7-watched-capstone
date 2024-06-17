4a5a1e3398979f5541379cc82ac99847dfe72a95
Michael Irvin <mirvin338@gmail.com>
2024-06-17 09:09:29 -0500

    Unfinished/Results and testing needed.

--- app/src/main/java/com/nashss/se/watched/activity/GetWatchlistsForUserActivity.java
+++ app/src/main/java/com/nashss/se/watched/activity/GetWatchlistsForUserActivity.java
@@ -2,14 +2,23 @@ package com.nashss.se.watched.activity;
 
 import com.nashss.se.watched.activity.request.GetWatchlistsForUserRequest;
 import com.nashss.se.watched.activity.results.GetWatchlistsForUserResult;
+import com.nashss.se.watched.converters.WatchlistConverter;
 import com.nashss.se.watched.dynamodb.WatchlistDao;
 
+import com.nashss.se.watched.dynamodb.models.Watchlist;
+import com.nashss.se.watched.models.WatchlistModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Activity to retrieve watchlists for a user.
  */
 public class GetWatchlistsForUserActivity {
+    private Logger log = LogManager.getLogger();
     private final WatchlistDao watchlistDao;
 
     /**
@@ -25,12 +34,22 @@ public class GetWatchlistsForUserActivity {
     /**
      * Handles the GetWatchlistsForUserRequest and returns a GetWatchlistsForUserResult.
      *
-     * @param request the request containing the user ID whose watchlists are to be retrieved
+     * @param getWatchlistsForUserRequest the request containing the user ID whose watchlists are to be retrieved
      * @return the result containing the retrieved watchlists
      */
-    public GetWatchlistsForUserResult handleRequest(GetWatchlistsForUserRequest request) {
+    public GetWatchlistsForUserResult handleRequest(final GetWatchlistsForUserRequest getWatchlistsForUserRequest) {
+        log.info("Handle Request Started for Get All Watchlists: Request ={}", getWatchlistsForUserRequest);
+
+        List<Watchlist> watchlists = watchlistDao.getWatchlistsForUser();
+        log.info("Retrieved Watchlists for User = {}", watchlists);
+
+        List<WatchlistModel> watchlistModels = new ArrayList<>();
+        for (Watchlist watchlist : watchlists) {
+            watchlistModels.add(WatchlistConverter.toWatchlistModel(watchlist));
+        }
+
         return GetWatchlistsForUserResult.builder()
-                .withWatchlists(watchlistDao.getWatchlistsForUser(request.getUserId()))
+                .withWatchlists(watchlistModels)
                 .build();
     }
 }
---
+++ app/src/main/java/com/nashss/se/watched/dynamodb/indexes/DynamoDBIndexConstants.java
@@ -0,0 +1,7 @@
+package com.nashss.se.watched.dynamodb.indexes;
+
+public class DynamoDBIndexConstants {
+    public static final String WATCHLIST_BY_USERID_INDEX = "WatchlistByUserIndex";
+
+}
+
