eaa3adf2a682a8c91766a47411aac04303a1d7bd
Michael Irvin <mirvin338@gmail.com>
2024-06-20 06:46:12 -0500

    displays added

--- web/src/pages/deleteWatchlist.js
+++ web/src/pages/deleteWatchlist.js
@@ -6,8 +6,12 @@ import DataStore from '../util/DataStore';
 class DeleteWatchlist extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'submit', 'clientLoaded'], this);
+        this.bindClassMethods(['mount', 'submit', 'clientLoaded', 'deleteWatchlistsDisplay'], this);
         this.dataStore = new DataStore();
+        this.dataStore.addChangeListener(this.deleteWatchlistsDisplay);
+        document.addEventListener('DOMContentLoaded', () => {
+            deleteWatchlistsDisplay();
+        });
         this.header = new Header(this.dataStore);
         console.log("deleteWatchlist constructor");
     }
@@ -16,7 +20,22 @@ class DeleteWatchlist extends BindingClass {
      * Once the client is loaded, get the watchlist metadata and title list.
      */
     async clientLoaded() {
-        console.log("Client loaded");
+        const errorMessageDisplay = document.getElementById('error-message');
+        errorMessageDisplay.innerText = ``;
+        errorMessageDisplay.classList.add('hidden');
+
+        try {
+            const watchlists = await this.client.getWatchlistsForUser();
+            if (watchlists) {
+                console.log("watchlists = ", watchlists);
+                this.dataStore.set('watchlists', watchlists);
+                console.log("Client loaded");
+            }
+        } catch (error) {
+            errorMessageDisplay.innerText = `Error: ${error.message}`;
+            errorMessageDisplay.classList.remove('hidden');
+        }
+        
     }
 
     mount() {
@@ -50,12 +69,36 @@ class DeleteWatchlist extends BindingClass {
             if (watchlist) { 
                 console.log("watchlist = ", watchlist);
                 this.dataStore.set('watchlist', watchlist);
-                console.log("Get Watchlist")
+                console.log("Get Watchlist");
                 const response = await this.client.deleteWatchlist(watchlist.id);
-                console.log("Deleted WatchlistId")
+                console.log("Deleted WatchlistId");
+                alert("Watchlist Deleted");
                 deleteButton.innerText = 'Delete';
         }
     }
+
+     /**
+     * Update the watchlists display with the fetched watchlist data.
+     * @param watchlist The watchlists data to display.
+     */
+     deleteWatchlistsDisplay() {
+        console.log("display1")
+        const watchlists = this.dataStore.get('watchlists');
+        
+        let rows = ""
+        for (const watchlist of watchlists) {
+            rows += "<li>" + watchlist.title + "<li/>"
+            rows += "<li>" + watchlist.id + "<li/>"
+        }
+        console.log("display2")
+        const watchlistsDisplay = document.getElementById('watchlists-display');
+         console.log("display3")
+
+        watchlistsDisplay.style.display = 'block';
+        watchlistsDisplay.innerHTML = rows;
+        console.log("displaycomplete")
+    }
+
 }
 
 const main = async () => {
